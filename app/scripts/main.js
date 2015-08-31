0c064e939c668cda911dff266bd47d14503defa2
Max Ehnert <max.ehnert@gmail.com>
2015-08-30 21:51:35 -0400

    removed redundate comments from main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,28 +1,4 @@
 "use strict";
-/*
-I originally found this chalenge on Interviewcake but never got to see the code for it. A quick search turned up a SO result from someone who built a very nice implementation of a word cloud and below is what was mentioned for how to build one. This was the only guildline I had for doing this. I'm certain my function is much more verbose than it needs to be (It goes string, array, object, array and many loops new new arrays) but it works and was fun doing. Perhaps I'll work on getting it tighter.
-------
-
-Count the words, throw away boring words, and sort by the count, descending. Keep the top N words for some N. Assign each word a font size proportional to its count. Generate a Java2D Shape for each word, using the Java2D API.
-
-Each word "wants" to be somewhere, such as "at some random x position in the vertical center". In decreasing order of frequency, do this for each word:
-
-place the word where it wants to be
-while it intersects any of the previously placed words
-    move it one step along an ever-increasing spiral
-That's it. The hard part is in doing the intersection-testing efficiently, for which I use last-hit caching, hierarchical bounding boxes, and a quadtree spatial index (all of which are things you can learn more about with some diligent googling).
-
-------
-
-Given a string of words. Create a word cloud from them.
-You'll want to loop over the string and count how many times each word occurs.
-Assign the top 10 words a font size, and remove the rest of the words.
-Print them out in some way.
-
-
-Yes my commenting is ugly.
-*/
-
 
 let wordCloud = string => {
 
@@ -75,15 +51,15 @@ let doSomethingWithTheArray = string => {
 
     var containerTop = $('.text-output-js').offset().top;
     var containerLeft = $('.text-output-js').offset().left;
-
+    console.log('top '+  (containerTop + Math.random() * 10));
     $('.text-output-js').append(`<div class='words' style='font-size:${fontSize}px'>${value[0]}</div>`)
-                        .offset({ top: containerTop + Math.random() * 10 , left: containerLeft + Math.random() * 10 });
+                        .offset({ top: containerTop + Math.random() * 10 ,
+                                  left: containerLeft + Math.random() * 10
+                               });
   };
 
   wordCloud(string).forEach(logMap);
 };
-
-doSomethingWithTheArray($('.text-input-js').val());
 
 $('.sumbit-btn-js').click( () => {
   doSomethingWithTheArray($('.text-input-js').val())
