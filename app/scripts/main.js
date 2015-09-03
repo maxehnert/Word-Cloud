47c1fe7b9356c6858d690d3e016ce1c512669e6e
Max Ehnert <max.ehnert@gmail.com>
2015-09-02 22:08:49 -0400

    started canvas space check

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,7 +7,7 @@ var context = canvas.getContext("2d");
 let wordCloud = string => {
 
   let sortedWords = [];
-  let word_object = {};
+  let wordObject = {};
   let word;
   /* Should think about using new Map() instead of using an object and then later creating the same key,value array. It could elminate a loop and rduce O space and time*/
 
@@ -19,23 +19,23 @@ let wordCloud = string => {
   	word = word.toLowerCase();
 
     // If it's a new word, add it to our object
-    if( !word_object[word] ) {
-      word_object[word] = 1;
+    if( !wordObject[word] ) {
+      wordObject[word] = 1;
     }
     // If it's Not a new word, just increment it
     else {
-    	word_object[word] += 1;
+    	wordObject[word] += 1;
     }
   }
 
   // Loop through our word object so you can put them into an array
-  for( word in word_object ) {
+  for( word in wordObject ) {
     // Setup some regex to stop any punctuation, numbers, or weird characters from getting through
     let validLetters = /[a-z]/;
 
     // Push each key,value into an array of arrays
     if( validLetters.test(word) ) {
-    	sortedWords.push( [word, word_object[word]] );
+    	sortedWords.push( [word, wordObject[word]] );
     }
   }
   // Use the built in sorting method to sort by value
@@ -46,24 +46,45 @@ let wordCloud = string => {
 };
 
 let doSomethingWithTheArray = string => {
+
   let countItems = 0;
+
   // Map over the array
   let logMap = (value, map) => {
 
     countItems = (map - countItems + 1);
 
-    // set a font size based on it's position in the array.
-    // The most frequent word will be the largest.
+
+
     var word = value[0];
-    var fontSize = value[1];
 
 
-    console.log(map);
+
+    /* set a font size based on it's position in the array.
+     * The most frequent word will be the largest.
+     * map count is incremental 0-9.
+     * 0 is the lowest count word
+     */
+    if (map == 0) {
+      var fontSize = map + 0.5;
+    } else var fontSize = map;
+
+
+  //  console.log(map);
     console.log(value);
     context.fillStyle = "hsl(" + Math.random() * 360 + ", 100%, 50%)";
-    context.font = `bold ${fontSize}px Arial`;
+    context.font = `bold ${fontSize}rem Arial`;
     context.fillText( word, Math.random() * 200 + 100 , Math.random() * 200 + 100 );
 
+    // trying to get dimensions.
+    var metrics = context.measureText(word);
+    var height = metrics.height;
+      var width = metrics.width;
+      var blah = metrics.actualBoundingBoxLeft;
+      console.log(metrics);
+      console.log('width '+ width);
+      console.log('actualBoundingBoxLeft '+ blah);
+      console.log('height '+ height);
   };
 
   wordCloud(string).forEach(logMap);
